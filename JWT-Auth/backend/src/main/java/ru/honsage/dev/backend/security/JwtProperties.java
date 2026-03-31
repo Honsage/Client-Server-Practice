58571529367d993e6a14003c71eae9beb28f4e75
Honsage <musatov.i.a@edu.mirea.ru>
2026-04-01 00:11:39 +0300

    feat(jwt-auth/backend): configure custom properties of jwt

---
+++ JWT-Auth/backend/src/main/java/ru/honsage/dev/backend/security/JwtProperties.java
@@ -0,0 +1,11 @@
+package ru.honsage.dev.backend.security;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtProperties {
+    private String secret;
+    private long expiration;
+}
