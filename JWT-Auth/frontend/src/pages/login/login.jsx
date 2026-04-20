589276f8ee8136e7aecf1ee09c61cfde09dfeafc
Honsage <musatov.i.a@edu.mirea.ru>
2026-04-20 17:36:36 +0300

    feat(jwt-auth/frontend): add login and register pages

---
+++ JWT-Auth/frontend/src/pages/login/login.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import AuthForm from '@components/auth-form/auth-form';
+
+const LoginPage = () => <AuthForm isRegister={false} />;
+export default LoginPage;
---
+++ JWT-Auth/frontend/src/pages/register/register.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import AuthForm from '@components/auth-form/auth-form';
+
+const RegisterPage = () => <AuthForm isRegister={true} />;
+export default RegisterPage;
